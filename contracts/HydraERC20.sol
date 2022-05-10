9760ab15b6f43082609409de03e4a4d3d34b635e
Andrew Chen <andrewrchen05@gmail.com>
2022-05-10 11:19:45 -0700

    Feat: Create HYDR contract

---
+++ contracts/HydraERC20.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract HydraERC20 is ERC20 {
+
+    address public vault;   // treasury vault
+
+    mapping(address => bool) isMinter;
+
+    constructor(uint256 initialSupply, address _vault) ERC20("Hydra", "HYDR") {
+        _mint(msg.sender, initialSupply);
+        vault = _vault;
+    }
+
+    modifier onlyVault() {
+        require(msg.sender == vault, "FORBIDDEN");
+        _;
+    }
+
+    function mint(address _to, uint256 _amount) external onlyVault returns (bool) {
+        _mint(_to, _amount);
+        return true;
+    }
+}
