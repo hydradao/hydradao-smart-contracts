303b4a8f604443500b33fc33334492885457ce96
Andrew Chen <andrewrchen05@gmail.com>
2022-05-16 21:45:29 -0700

    Add typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import { HardhatUserConfig } from "hardhat/config";
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  // Your type-safe config goes here
   solidity: "0.8.0",
 };
+
+export default config;
